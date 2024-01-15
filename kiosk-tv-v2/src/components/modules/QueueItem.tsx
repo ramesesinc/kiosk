859ec2d3ec402e780bd1e67d3bf93bc73beb7c2f
James Kevin Jamili <jameskevin.ramesessystems@gmail.com>
2024-01-15 11:38:45 +0800

    add kiosk-tv

---
+++ kiosk-tv-v2/src/components/modules/QueueItem.tsx
@@ -0,0 +1,48 @@
+// QueueItem.tsx
+import React from "react";
+import SubTitle from "../ui/SubTitle";
+import Number from "../ui/Number";
+
+export type QueueItemProps = {
+  queueSection?: number;
+  queueTicket?: number;
+  counter: number;
+};
+
+const QueueItem: React.FC<QueueItemProps> = ({
+  counter,
+  queueTicket,
+  queueSection,
+}) => {
+  return (
+    <div className="bg-white w-[300px] rounded-xl shadow-md border-2 p-2 max-md:w-[200px] ">
+      <div className="flex flex-row gap-2 h-24 max-md:h-16">
+        <div className="flex flex-col justify-center items-center text-center p-[5px]">
+          <div className="w-20 max-md:w-2">
+            <Number
+              number={counter}
+              className="text-5xl max-xl:text-3xl max-md:text-2xl"
+            />
+            <SubTitle
+              text={"Counter"}
+              className="uppercase text-[14px] max-xl:text-[11px] max-md:text-[9px]"
+            />
+          </div>
+        </div>
+        <div className="border border-black border-solid h-14 mt-3 mr-8 max-md:mr-0 max-md:mt-0 max-md:h-8 max-md:ml-5"></div>
+        <div className="flex flex-col justify-center items-center text-center w-full p-[5px]">
+          <Number
+            number={queueTicket}
+            className="text-5xl max-xl:text-3xl max-md:text-2xl"
+          />
+          <SubTitle
+            text={queueSection}
+            className="uppercase text-[14px] max-xl:text-[11px] max-md:text-[9px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QueueItem;
