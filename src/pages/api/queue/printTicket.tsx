59a65dc18819af7b404357f8a165e1c5d3738c7e
meg <megp.rameses@gmail.com>
2024-03-21 09:50:11 +0800

    updated queue print ticket

--- src/pages/api/queue/printTicket.tsx
+++ src/pages/api/queue/printTicket.tsx
@@ -6,51 +6,52 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    await POST(req, res); // Call the POST handler
+    await POST(req, res);
   } else {
     res.status(500).json({ code: "01", error: "Method not available." });
   }
 }
 
 async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const space = (count: number): string => {
-    return " ".repeat(count);
-  };
-
-  const countCharacter = (str: string, alignment: string, label: string) => {
-    let grid = 48;
-    let padding = 0;
-
-    if (alignment === "left") {
-      " ".repeat(0);
-    } else if (alignment === "center") {
-      padding = Math.floor((grid - str.length) / 2);
-      return `${space(padding)}${str}`;
-    } else if (alignment === "right") {
-      padding = grid - str.length - label.length;
-      return `${space(padding)}${str}`;
-    }
-  };
+  const EscPosEncoder = require("esc-pos-encoder");
 
   try {
-    const title = "Queue Ticket Number";
-    const valid = "This is number is valid only on";
+    const lguName = process.env.LGU_NAME;
     const recieveTicketInfo = req.body;
-    const escposText = `\x1D\x21\x11${space(3)}${title}
-\x1D\x21\x01${countCharacter(recieveTicketInfo.subtitle, "center", "")}
-\x0A
-\x1D\x21\x44${space(3)}${recieveTicketInfo.ticketno}
-\x0A
-\x1D\x21\x01${countCharacter(valid, "center", "")}
-\x1D\x21\x01${countCharacter(recieveTicketInfo.date, "center", "")}
-\x0A
-\x0A
-\x1D\x56\x00
-  `;
+    const encoder = new EscPosEncoder();
+    const commands = encoder
+      .initialize()
+      .align("center")
+      .width(2)
+      .height(2)
+      .bold()
+      .text("Queue Ticket Number")
+      .bold()
+      .newline()
+      .width(1)
+      .height(2)
+      .bold()
+      .text(lguName)
+      .newline()
+      .width(5)
+      .height(5)
+      .bold()
+      .text(`${recieveTicketInfo.ticketno}`)
+      .newline()
+      .width(1)
+      .height(1)
+      .text("This is number is valid only on")
+      .newline()
+      .text(`${recieveTicketInfo.date}`)
+      .newline()
+      .newline()
+      .newline()
+      .cut("full")
+      .encode();
 
     const printResponse = await axios.post(
       `http://${process.env.KIOSK_PRINT_SEVER_IP}:11111/api/print`,
-      escposText
+      commands
     );
     res
       .status(200)
